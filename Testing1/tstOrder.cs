b3acf55c7d5ae561842e9c92d1689990f58008fe
Leorev01 <revrennaleo@gmail.com>
2024-05-05 17:08:23 +0100

    added tstOrder and clsorder class all completed

---
+++ Testing1/tstOrder.cs
@@ -0,0 +1,18 @@
+﻿using ClassLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Testing1
+{
+    [TestClass]
+    public class tstOrder
+    {
+        [TestMethod]
+        public void InstanceOK()
+        {
+            clsOrder anOrder = new clsOrder();
+
+            Assert.IsNotNull(anOrder);
+        }
+    }
+}
