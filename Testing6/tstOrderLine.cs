a712a8db9784cd81d59f9ec4dc4d0040402a12c4
Leorev01 <revrennaleo@gmail.com>
2024-05-06 20:35:57 +0100

    Added OrderLine, fixed a mistake i made on stock and done stocktest as I made mistake

---
+++ Testing6/tstOrderLine.cs
@@ -0,0 +1,19 @@
+﻿using ClassLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Testing6
+{
+    [TestClass]
+    public class tstOrderLine
+    {
+        [TestMethod]
+        public void InstanceOK()
+        {
+
+            clsOrderLine AnOrderLine = new clsOrderLine();
+
+            Assert.IsNotNull(AnOrderLine);
+        }
+    }
+}
