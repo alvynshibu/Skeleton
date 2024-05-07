d28bdfb2d4a6811f6a434aa5e7cb5d96c65b7aca
alvynshibu <alvynshibu@gmail.com>
2024-05-07 10:33:39 +0100

    Added all properties for clsCustomer and the tests that accompany them in tstCustomer

--- ClassLibrary/clsCustomer.cs
+++ ClassLibrary/clsCustomer.cs
@@ -1,6 +1,15 @@
-﻿namespace ClassLibrary
+﻿using System;
+
+namespace ClassLibrary
 {
     public class clsCustomer
     {
+        public bool Email { get; set; }
+        public DateTime RegistrationDate { get; set; }
+        public int CustomerId { get; set; }
+        public string CustomerName { get; set; }
+        public string CustomerEmail { get; set; }
+        public int PhoneNumber { get; set; }
+        public string CustomerAddress { get; set; }
     }
 }
