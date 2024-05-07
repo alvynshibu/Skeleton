77262b5263ae71fa42110f4b7bd31dcdfaf708f1
Leorev01 <revrennaleo@gmail.com>
2024-05-07 09:57:34 +0100

    added couple properties to order

--- Testing2/tstOrder.cs
+++ Testing2/tstOrder.cs
@@ -14,5 +14,44 @@ namespace Testing1
 
             Assert.IsNotNull(anOrder);
         }
+
+        [TestMethod]
+
+        public void DeliveryStatusOK()
+        {
+            clsOrder anOrder = new clsOrder();
+
+            Boolean TestData = true;
+
+            anOrder.DeliveryStatus = TestData;
+
+            Assert.AreEqual(anOrder.DeliveryStatus, TestData);
+        }
+
+        [TestMethod]
+
+        public void OrderDateOK()
+        {
+            clsOrder anOrder = new clsOrder();
+
+            DateTime TestData = DateTime.Now.Date;
+
+            anOrder.DateAdded = TestData;
+
+            Assert.AreEqual(anOrder.DateAdded, TestData);
+        }
+
+        [TestMethod]
+
+        public void CustomerIdOK()
+        {
+            clsOrder anOrder = new clsOrder();
+
+            Int TestData = 1;
+
+            anOrder.CustomerId = TestData;
+
+            Assert.AreEqual(anOrder.CustomerId, TestData);
+        }
     }
 }
