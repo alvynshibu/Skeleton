093d11f491c028a0425c52d6962d2a4c58762760
zia hassankhail <p2760380@GWHXX283WIN010W>
2024-05-09 12:45:25 +0100

    lab 8 completed

--- AdminSystem/StockDataEntry.aspx.cs
+++ AdminSystem/StockDataEntry.aspx.cs
@@ -4,11 +4,34 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using ClassLibrary;
 
 public partial class _1_DataEntry : System.Web.UI.Page
 {
+
     protected void Page_Load(object sender, EventArgs e)
     {
 
     }
+
+
+
+
+
+    protected void btnOK_Click(object sender, EventArgs e)
+    {
+        clsStock aStock = new clsStock();
+        aStock.StockId = Convert.ToInt32(txtStockId.Text);   
+        aStock.ItemName = txtItemName.Text;
+        aStock.Quantity = Convert.ToInt32(txtQuantity.Text);
+        aStock.Price = Convert.ToDecimal(txtPrice.Text); 
+        aStock.SupplierId = Convert.ToInt32(txtSupplierId.Text);
+        aStock.Available = CheckBox1.Checked;
+        aStock.DateAdded = Convert.ToDateTime(DateTime.Now);
+        aStock.Active = CheckBox1.Checked; 
+        Session["aStock"] = aStock;
+        Response.Redirect("StockViewer.aspx");
+    }
+
+ 
 }
--- AdminSystem/StockViewer.aspx.cs
+++ AdminSystem/StockViewer.aspx.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ClassLibrary;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,6 +10,17 @@ public partial class _1Viewer : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
+        clsStock aStock = new clsStock();
+        aStock = (clsStock)Session["aStock"];
+        Response.Write(aStock.StockId);
+        Response.Write(aStock.ItemName);
+        Response.Write(aStock.Quantity);
+        Response.Write(aStock.Price);
+        Response.Write(aStock.SupplierId);
+        Response.Write(aStock.Available);
+        Response.Write(aStock.DateAdded);
+
 
     }
+
 }
