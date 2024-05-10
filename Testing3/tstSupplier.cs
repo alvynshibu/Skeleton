aecdb8c88bc29c1cbc13839b7efa4b945319ea69
Uzair793 <uzairakbar2003@gmail.com>
2024-05-10 05:20:11 +0100

    Added testing for supplier class

---
+++ Testing3/tstSupplier.cs
@@ -0,0 +1,19 @@
+﻿using ClassLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Testing3
+{
+    [TestClass]
+    public class tstSupplier
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            //create an insstance of the class we want to create
+            clsSupplier aSupplier = new clsSupplier();
+            //test to see that it exists
+            Assert.IsNotNull(aSupplier);
+        }
+    }
+}
