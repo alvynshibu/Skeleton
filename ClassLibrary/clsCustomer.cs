6005c925f731f9f1f978a92467d4814286d99f66
alvynshibu <alvynshibu@gmail.com>
2024-05-16 09:54:12 +0100

    Added all validation methods and are all passing

--- ClassLibrary/clsCustomer.cs
+++ ClassLibrary/clsCustomer.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Data.Common;
 using System.Diagnostics.SymbolStore;
 
 namespace ClassLibrary
@@ -11,7 +12,7 @@ namespace ClassLibrary
         private DateTime mRegistrationDate;
         private string mCustomerName;
         private string mCustomerEmail;
-        private Int32 mPhoneNumber;
+        private string mPhoneNumber;
         private string mCustomerAddress;
 
 
@@ -93,7 +94,7 @@ namespace ClassLibrary
 
 
 
-        public Int32 PhoneNumber
+        public string PhoneNumber
         {
             get
             {
@@ -141,7 +142,7 @@ namespace ClassLibrary
                 mCustomerEmail = Convert.ToString(DB.DataTable.Rows[0]["CustomerEmail"]);
                 mCustomerName = Convert.ToString(DB.DataTable.Rows[0]["CustomerName"]);
                 mCustomerAddress = Convert.ToString(DB.DataTable.Rows[0]["CustomerAddress"]);
-                mPhoneNumber = Convert.ToInt32(DB.DataTable.Rows[0]["PhoneNumber"]);
+                mPhoneNumber = Convert.ToString(DB.DataTable.Rows[0]["PhoneNumber"]);
                 mRegistrationDate = Convert.ToDateTime(DB.DataTable.Rows[0]["RegistrationDate"]);
                 mEmailNotification = Convert.ToBoolean(DB.DataTable.Rows[0]["EmailNotification"]);
                 //return everything that worked ok
@@ -154,6 +155,99 @@ namespace ClassLibrary
                 return false;
             }
         }
+
+        public string Valid(string customerName, string customerAddress, string customerEmail, string phoneNumber, string registrationDate)
+        {
+            //create a string variable to store the error
+            String Error = "";
+            //create a temporary variable to store the date values
+            DateTime DateTemp;
+            //if the CustomerName is blank
+            if (customerName.Length == 0)
+            {
+                //record the error
+                Error = Error + "The customer name may not be blank  : ";
+            }
+            //if the customer name is greater than 30 characters
+            if (customerName.Length > 30)
+            {
+                //record the error
+                Error = Error + "The customer name must be less than 30 characters  : ";
+            }
+            try
+            {
+                //copy the registrationDate value to the DateTemp variable
+                DateTemp = Convert.ToDateTime(registrationDate);
+
+                //check to see if the date is less than today's date
+                if (DateTemp < DateTime.Now.Date)
+                {
+                    Error = Error + "The date cannot be in the past  : ";
+                }
+                //check to see if the date is greater than today's date
+                if (DateTemp > DateTime.Now.Date)
+                {
+                    //record the error
+                    Error = Error + "The date cannot be in the future  : ";
+                }
+            }
+            catch
+            {
+                //record the error
+                Error = Error + "The date was not a valid date  :";
+            }
+
+            //if the CustomerEmail is blank
+            if (customerEmail.Length == 0)
+            {
+                //record the error
+                Error = Error + "The customer email may not be blank  : ";
+            }
+
+            //if the customer email is too long
+            if (customerEmail.Length > 45)
+            { 
+                //record the error
+                Error = Error + "The customer email must be less than 45 characters  : ";
+            }
+
+            //if the customer address is blank
+            if (customerAddress.Length == 0)
+            {
+                //record the error
+                Error = Error + "The customer address may not be blank  : ";
+            }
+
+            //if the customer address is too long
+            if (customerAddress.Length > 255)
+            {
+                //record the error
+                Error = Error + "The customer address must be less than 300 characters  : ";
+            }
+
+            //if the phone number is blank
+            if (phoneNumber.Length == 0)
+            {
+                //record the error
+                Error = Error + "The phone number may not be blank";
+            }
+
+            //if the phone number is too long
+            if (phoneNumber.Length > 15)
+            {
+                //record the error
+                Error = Error + "The phone number must be less than 15 characters  : ";
+            }
+
+
+            //return any error messages
+            return Error;
+        }
+
+
+
+
+
     }
 
 }
