61241a86b603b4e0844f580f668fb16719b59cf4
zia hassankhail <p2760380@GWHXX283WIN010W>
2024-05-28 12:18:54 +0100

    aa

---
+++ ClassLibrary/clsStockUser.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Diagnostics.Eventing.Reader;
+
+namespace ClassLibrary
+{
+    public class clsStockUser
+    {
+        //private data member for the user id propety
+        private Int32 mUserId;
+        //private data member for the user name property
+        private String mUserName;
+        //private data member for the password property
+        private String mPassword;
+        //private data member for the department property
+        private String mDepartment;
+
+        public int UserId
+        {
+            get
+            {
+                return mUserId;
+            }
+            set
+            {
+                mUserId = value;
+            }
+        }
+        public string UserName
+        {
+            get
+            {
+                return mUserName;
+            }
+            set
+            {
+                mUserName = value;
+            }
+        }
+        public string Password
+        {
+            get
+            {
+                return mPassword;
+            }
+            set
+            {
+                mPassword = value;
+            }
+        }
+        public string Department
+        {
+            get
+            {
+                return mDepartment;
+            }
+            set
+            {
+                mDepartment = value;
+            }
+        }
+
+        public bool FindUser(string UserName, string Password)
+        {
+            //create an instance of the data connection
+            clsDataConnection DB = new clsDataConnection();
+            //add the parameters for the user username and password to search for
+            DB.AddParameter("@UserName", UserName);
+            DB.AddParameter("@Password", Password);
+            //execute the stored procedure
+            DB.Execute("sproc_tblUsers_FindUserNamePW");
+            //if one record is found (there should be either one or none)
+            if (DB.Count == 1)
+            {
+                //copy the data from the database to the private data members
+                mUserId = Convert.ToInt32(DB.DataTable.Rows[0]["UserId"]);
+                mUserName = Convert.ToString(DB.DataTable.Rows[0]["UserName"]);
+                mPassword = Convert.ToString(DB.DataTable.Rows[0]["Password"]);
+                mDepartment = Convert.ToString(DB.DataTable.Rows[0]["Department"]);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+                }
+            }
+
+        }
+    
---
+++ Testing1/tstStockUser.cs
@@ -0,0 +1,80 @@
+﻿using ClassLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Testing1
+{
+    [TestClass]
+    public class tstStockUser
+    {
+        [TestMethod]
+        public void InstanceOK()
+        {
+            clsStockUser AnUser = new clsStockUser();
+            Assert.IsNotNull(AnUser);
+        }
+        [TestMethod]
+        public void UserIdPropertyOK()
+        {
+            clsStockUser AnUser = new clsStockUser();
+            Int32 TestData = 1;
+            AnUser.UserId = TestData;
+            Assert.AreEqual(AnUser.UserId, TestData);
+        }
+
+        [TestMethod]
+        public void UserNamePropertyOK()
+        {
+            clsStockUser AnUser = new clsStockUser();
+            string TestData = "zia";
+            AnUser.UserName = TestData;
+            Assert.AreEqual(AnUser.UserName, TestData);
+        }
+
+        [TestMethod]
+        public void PasswordPropertyOK()
+        {
+            clsStockUser AnUser = new clsStockUser();
+            string TestData = "zia123";
+            AnUser.Password = TestData;
+            Assert.AreEqual(AnUser.Password, TestData);
+        }
+
+        [TestMethod]
+        public void DepartmentPropertyOK()
+        {
+            clsStockUser AnUser = new clsStockUser();
+            string TestData = "StockList";
+            AnUser.Department = TestData;
+            Assert.AreEqual(AnUser.Department, TestData);
+        }
+
+        [TestMethod]
+        public void FindUserMethodOK()
+        {
+            clsStockUser AnUser = new clsStockUser();
+            Boolean Found = false;
+            string UserName = "Zia";
+            string Password = "Zia123";
+            Found = AnUser.FindUser(UserName, Password);
+            Assert.IsTrue(Found);
+        }
+
+        [TestMethod]
+        public void TestUserNamePWFound()
+        {
+            clsStockUser AnUser = new clsStockUser();
+            Boolean Found = false;
+            Boolean OK = true;
+            string UserName = "Zia";
+            string Password = "Zia123";
+            Found = AnUser.FindUser(UserName, Password);
+            if (AnUser.UserName != UserName && AnUser.Password != Password)
+            {
+                OK = false;
+            }
+            Assert.IsTrue(OK);
+        }
+
+    }
+}
