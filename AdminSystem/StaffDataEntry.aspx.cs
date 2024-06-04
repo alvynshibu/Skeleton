1a992b767d11943453662647d3f5f81e27d38277
p2767838 <mdsakibsikdertibro@gmail.com>
2024-06-04 22:34:20 +0100

    updted 3.2

--- AdminSystem/StaffDataEntry.aspx.cs
+++ AdminSystem/StaffDataEntry.aspx.cs
@@ -21,7 +21,35 @@ public partial class _1_DataEntry : System.Web.UI.Page
         AnStaff.Name = txtName.Text;
         Session["AnStaff"] = AnStaff;
         //navigate to the view page
+
+
         Response.Redirect("StaffViewer.aspx");
+
+        //capture the notificaion
+        AnStaff.Notification=chkNotification.Checked;
+        ;
+
+        //capture the Date Added
+        AnStaff.DateOfBirth = Convert.ToDateTime(DateTime.Now);
+       
+
+        //Email
+        AnStaff.Email = txtEmail.Text;
+       
+
+
+
+        //Address
+        AnStaff.Address = txtAddress.Text;
+        
+
+
+        Response.Redirect("StaffViewer.aspx");
+
+
+
+
+
     }
 
     protected void txtName_TextChanged(object sender, EventArgs e)
