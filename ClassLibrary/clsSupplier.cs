de1944f35c285abf8cce92f2e1fb5f1129a131a5
Uzair793 <uzairakbar2003@gmail.com>
2024-06-07 05:51:03 +0100

    Initial tests

--- ClassLibrary/clsSupplier.cs
+++ ClassLibrary/clsSupplier.cs
@@ -2,5 +2,13 @@
 {
     public class clsSupplier
     {
+        public string Email;
+
+        public bool Active { get; set; }
+        public string Name { get; set; }
+        public string ContactPerson { get; set; }
+        public int SupplierId { get; set; }
+        public string Phone { get; set; }
+        public string Address { get; set; }
     }
 }
